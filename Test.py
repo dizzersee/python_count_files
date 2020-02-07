4539dee14ed232df3cc69ba375ddbfb666bf3c86
dizzersee <dizzersee@gmail.com>
2020-02-07 13:57:26 +0100

    Refactoring

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ import glob
 import os
 import json
 from lib import ExtensionEntriesDictionary
+from lib import AnalysisData
 import lib
 
 # TODO read values in
@@ -11,14 +12,23 @@ output_file_path = "output.json"  # this will be output
 abs_dir_path = os.path.abspath(dir_path)
 print("\nAnalyzing directory " + abs_dir_path + "\n\n")
 
-extension_entries_dict = ExtensionEntriesDictionary()
+analysisData = AnalysisData()
+extension_entries_dict = analysisData.entries_dict
+
+# Loop through files
 
 for filename in glob.glob(abs_dir_path + "/**/*", recursive=True):
     print(filename)
     if not os.path.isfile(filename): continue  # Skip directories
-    file_extension = os.path.splitext(filename)[1]
+
+    # Get entry for filename
+
+    file_extension = os.path.splitext(filename)[1]  # TODO brauchen z.B. .blade.php einzeln von .php?
+    if file_extension == "":  # todo test if works
+        analysisData.files_without_extension += 1
+        continue  # todo add this to json output
+
     currentEntry = extension_entries_dict.get_entry(file_extension)
-    currentEntry.files_count += 1
+    currentEntry.add_file_data(filename)
 
-extension_entries_dict.write_output_to_file(output_file_path)
-
+analysisData.write_output_to_file(output_file_path)
--- lib.py
+++ lib.py
@@ -1,21 +1,27 @@
 import json
 
-
-class ExtensionEntry:
-    characters = 0
-    files_count = 0
-    CHARACTERS_PER_LINE = 60  # TODO brauchen beleg
-
-    def __init__(self, extension):
-        self.extension = extension
+class Serializable:
 
     def toJSON(self):
         return self.__dict__
 
 
-class ExtensionEntriesDictionary:
+class ExtensionEntry(Serializable):
+    CHARACTERS_PER_LINE = 60  # TODO brauchen beleg
+
+    def __init__(self, extension):
+        self.extension = extension
+        self.characters = 0
+        self.files_count = 0
+
+    def add_file_data(self, filename):
+        self.files_count += 1
+
+
+class ExtensionEntriesDictionary(Serializable):
 
     def __init__(self):
+        self.extensions_count = 0
         self.entries = {}
 
     def get_entry(self, file_extension):
@@ -24,12 +30,18 @@ class ExtensionEntriesDictionary:
         else:
             current_entry = ExtensionEntry(file_extension)
             self.entries[file_extension] = current_entry
+            self.extensions_count +=1
         return current_entry
 
+
+class AnalysisData(Serializable):
+
+    def __init__(self):
+        self.entries_dict = ExtensionEntriesDictionary()
+        self.files_without_extension = 0
+
     def write_output_to_file(self, output_file_path):
-        json_output = json.dumps(self.entries, default=lambda object: object.toJSON(), indent=4)
+        json_output = json.dumps(self, default=lambda object: object.toJSON(), indent=4)
         f = open(output_file_path, "w")
         f.write(json_output)
         f.close()
-
-
